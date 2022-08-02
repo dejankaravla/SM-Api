14e88a19b646a981d68b3b778ccc2c9fe3d58e34
dejankaravla <karavla013@gmail.com>
2022-08-02 20:04:32 +0200

    cors 2

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ export const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://dashboardsmmobile.web.app/"],
+    origin: "https://dashboardsmmobile.web.app/",
   })
 );
 
