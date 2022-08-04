00e654b76c347d7c0b3aa31df68177b0800b71e3
dejankaravla <karavla013@gmail.com>
2022-08-04 18:29:27 +0200

    Changed back to local

--- src/index.js
+++ src/index.js
@@ -1,41 +1,33 @@
-import http from 'http'
-import mongoose from 'mongoose'
-import 'dotenv/config'
-import { app } from './app.js'
-import { MongoClient, ServerApiVersion } from 'mongodb'
+import http from "http";
+import mongoose from "mongoose";
+import "dotenv/config";
+import { app } from "./app.js";
 
-const uri = "mongodb+srv://heroku:heroku@cluster0.xrgkt.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
+// Mongo Atlas
+// import { MongoClient, ServerApiVersion } from 'mongodb'
+// const uri = "mongodb+srv://heroku:heroku@cluster0.xrgkt.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// const MONGO_URL = "mongodb://localhost:27017/SM"
 
 const PORT = process.env.PORT || 80;
+const MONGO_URL = process.env.MONGO_URL;
 
+const server = http.createServer(app);
 
-const MONGO_URL = "mongodb://localhost:27017/SM"
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready");
+});
 
-const server = http.createServer(app)
-
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
-mongoose.connection.once('open', () => {
-  console.log('MongoDB connection ready');
-})
-
-mongoose.connection.on('error', (err) => {
+mongoose.connection.on("error", (err) => {
   console.log(err);
-})
+});
 
 const startServer = async () => {
-  await mongoose.connect(uri)
+  await mongoose.connect(MONGO_URL);
 
   server.listen(PORT, () => {
     console.log(`Server is listening on port ${process.env.PORT} `);
-  })
-}
+  });
+};
 
-startServer()
+startServer();
